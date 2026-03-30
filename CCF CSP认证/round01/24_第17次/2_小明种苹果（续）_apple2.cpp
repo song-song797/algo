2fc5c3e702ddb5dd99eecb101b0a73df3adc6d63
song-song797 <2276071652@qq.com>
2026-03-30 16:03:19 +0800

    update#

--- CCF CSP认证/round01/24_第17次/2_小明种苹果（续）_apple2.cpp
+++ CCF CSP认证/round01/24_第17次/2_小明种苹果（续）_apple2.cpp
@@ -3,6 +3,48 @@
  * CCF CSP 第17次认证 真题2
  */
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+bool is_drop[1005];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    int D = 0;
+    int T = 0;
+    for (int i = 0; i < N; i++) {
+        int num;
+        cin >> num;
+        int curr;
+        cin >> curr;
+        bool valid = false;
+        for (int j = 1; j <= num - 1; j++) {
+            int m;
+            cin >> m;
+            if (m <= 0) {
+                curr += m;
+            } else {
+                if (curr != m) {
+                    curr = m;
+                    valid = true;
+                }
+            }
+        }
+        T += curr;
+        if (valid) {
+            D++;
+            is_drop[i] = true;
+        }
+    }
+    int E = 0;
+    for (int i = 0; i < N;i++){
+        if(is_drop[i]&&is_drop[(i+1)%N]&&is_drop[(i+2)%N]){
+            E++;
+        }
+    }
+    cout << T << ' ' << D << ' ' << E << '\n';
+
+    return 0;
+}
--- CCF CSP认证/round01/24_第17次/3_字符画_ascii.cpp
+++ CCF CSP认证/round01/24_第17次/3_字符画_ascii.cpp
@@ -3,6 +3,14 @@
  * CCF CSP 第17次认证 真题3
  */
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int m, n;
+    cin >> m >> n;
+    int p, q;
+    cin >> p >> q;
+    return 0; 
+}
